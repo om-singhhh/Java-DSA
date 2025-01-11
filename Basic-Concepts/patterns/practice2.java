e7b1c6baf9324cba1b0d2541a02269a7b8efd2bd
OmSingh04 <155307407+OmSingh04@users.noreply.github.com>
2025-01-11 22:02:23 +0530

    change

--- Basic-Concepts/patterns/practice2.java
+++ Basic-Concepts/patterns/practice2.java
@@ -3,11 +3,23 @@ public class practice2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n =sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print(j);
+        int count =1;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
-            System.out.println(" ");
+            for(int j=1;j<=i;j++){
+                System.out.print(count);
+                count++;
+            }
+            for(int j=i;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(count);
+              
+            }
+        System.out.println("");   
         }
         sc.close();
     }
