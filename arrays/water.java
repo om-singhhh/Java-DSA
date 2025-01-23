bb57b9f7d08992471daa1408b5a0d099e40311d4
OmSingh04 <155307407+OmSingh04@users.noreply.github.com>
2025-01-23 11:47:27 +0530

    if-while
    i knew learn new thing in this about if and while conditional statement

---
+++ arrays/water.java
@@ -0,0 +1,17 @@
+public class water {
+    public static void main(String[] args) {
+        int numBottles=9;
+        int numExchange =3;
+        int ans = numBottles;
+        while(numBottles>=numExchange){
+            int newBottles=numBottles/numExchange;
+            int remBottles =numBottles%numExchange;
+            ans = ans+newBottles;
+            numBottles = newBottles+remBottles;
+        }
+        System.out.println(ans);
+                
+    
+    }
+}
+
