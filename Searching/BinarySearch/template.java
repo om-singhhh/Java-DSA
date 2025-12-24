8bda70ea159fac87478cb60faba2bbe617bae885
OmSingh04 <omsingh18609@gmail.com>
2025-12-24 23:44:51 +0530

    Binary Search ALGO

---
+++ Searching/BinarySearch/template.java
@@ -0,0 +1,45 @@
+package Searching.BinarySearch;
+public class template {
+    static int binarySearch(int []arr, int target){
+        int start = 0 ;
+        int end = arr.length - 1;
+
+        boolean isAsc;
+        if(arr[start] < arr[end]){
+            isAsc = true;
+        }
+        else{
+           isAsc=false;
+        }
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) { // ascending order
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else { // descending order
+                if (target < arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int [] nums={1,2,3,4,5,6,7,8,9};
+        int target = 5;
+        System.out.println(binarySearch(nums, target));
+    }
+}
