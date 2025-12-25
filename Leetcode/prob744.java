1b0b624320f34c71c696df9a2be0e3eb6c0533c3
OmSingh04 <omsingh18609@gmail.com>
2025-12-25 19:58:59 +0530

    list of leetcode questions

---
+++ Leetcode/prob744.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class prob744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0;
+        int h = letters.length - 1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (letters[mid] <= target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+
+        return letters[l % letters.length];
+    }
+    public static void main(String[] args) {
+        prob744 obj = new prob744();
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        System.out.println(obj.nextGreatestLetter(letters, target));
+    }
+}
