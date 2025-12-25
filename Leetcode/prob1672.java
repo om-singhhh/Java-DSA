ca89612d73c15e5b6e97bd660f91b8c87537a9e0
OmSingh04 <omsingh18609@gmail.com>
2025-12-25 20:03:42 +0530

    practised ques

---
+++ Leetcode/prob1672.java
@@ -0,0 +1,22 @@
+package Leetcode;
+public class prob1672 {
+    // LeetCode 1672: Richest Customer Wealth
+    public static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] customer : accounts) {
+            int sum = 0;
+            for (int wealth : customer) sum += wealth;
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // example usage
+        int[][] accounts = {
+            {1, 2, 3},
+            {3, 2, 1}
+        };
+        System.out.println(maximumWealth(accounts)); // expected 6
+    }
+}
