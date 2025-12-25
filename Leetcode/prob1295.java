529cfdc6eba0c8f43e27fd933bf31cde79869fd7
OmSingh04 <omsingh18609@gmail.com>
2025-12-25 20:01:26 +0530

    previous praticed ques

---
+++ Leetcode/prob1295.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+public class prob1295 {
+    class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int num : nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    static int digits(int num){
+        int count=0;
+        while(num>0){
+            count++;
+            num=num/10;    
+        }
+        return count;
+    }
+    static boolean even(int num){
+        int numofDigits=digits(num);
+        if(numofDigits%2==0){
+            return true;
+        }
+        return false;
+    }
+
+}    public static void main(String[] args) {
+        prob1295 obj = new prob1295();
+        Solution sol = obj.new Solution();
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(sol.findNumbers(nums));
+    }   
+}
