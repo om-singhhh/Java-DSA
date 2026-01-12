628f5c4e41cbb63a0c0fb738be5adea056e4ebc0
OmSingh04 <omsingh18609@gmail.com>
2026-01-12 12:49:18 +0530

    practice ques on logic building

---
+++ Leetcode/prob287.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int s = 1;
+        int e = nums.length-1;
+        while(s<e){
+            int mid = s + (e-s)/2;
+            int cnt = 0 ;
+            for(int i =0;i<nums.length;i++){
+                if(nums[i]<=mid){
+                    cnt++;
+                }    
+            }
+            if(cnt<=mid){
+                    s = mid+1;
+                }
+                else{
+                    e = mid;
+                }
+        }
+        return s;
+    }
+}
